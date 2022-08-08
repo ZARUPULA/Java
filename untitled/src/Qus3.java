5bb9d8cbba4f986398cf0f3168bb576bda30a0e5
Zarupula Karthik <Zarupula.Karthik@ivycomptech.com>
2022-08-08 18:27:48 +0530

    3 qus

--- untitled/src/Qus3.java
+++ untitled/src/Qus3.java
@@ -2,45 +2,22 @@ import java.util.Scanner;
 
 public class Qus3 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the number of rows: ");
-
-        int rows = sc.nextInt();
-
-        for (int i = 1; i <= rows; i++)
-        {
-            int num;
-
-            if(i%2 == 0)
-            {
-                num = 0;
-
-                for (int j = 1; j <= rows; j++)
-                {
-                    System.out.print(num);
-
-                    num = (num == 0)? 1 : 0;
-                }
+        String k = sc.next();
+        String a = sc.next();
+        int l = sc.nextInt();
+        for (int i=0;i<l;i++){
+            for (int j=0;j<=i;j++) {
+                System.out.print(k+" ");
             }
-            else
-            {
-                num = 1;
-
-                for (int j = 1; j <= rows; j++)
-                {
-                    System.out.print(num);
-
-                    num = (num == 0)? 1 : 0;
-                }
+            for (int k=l-i-1; k>0;k--) {
+                System.out.print(a+" ");
             }
-
             System.out.println();
         }
-
-        sc.close();
     }
 }
+
+
 
