4631eb3224a6ccccbb837ca51d932e707c7afc0c
Zarupula Karthik <Zarupula.Karthik@ivycomptech.com>
2022-08-08 22:44:34 +0530

    h

--- untitled/src/Qus5.java
+++ untitled/src/Qus5.java
@@ -1,3 +1,23 @@
-public class Qus5 {
-    public
-}
+
+    import java.util.Scanner;
+    public class Qus5 {
+
+            public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int inputNo = sc.nextInt();
+
+                while(inputNo != 1){
+                    System.out.print(inputNo + " ->");
+                    if(inputNo % 2 == 0){
+                        inputNo /= 2;
+                    }else{
+                        inputNo = (3*inputNo) + 1;
+                    }
+                }
+                System.out.println(1);
+            }
+        }
+
+
+
+
