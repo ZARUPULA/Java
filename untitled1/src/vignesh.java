49d0f2295761223de180f1b5e46df31e56f9e201
Zarupula Karthik <Zarupula.Karthik@ivycomptech.com>
2022-08-10 12:02:14 +0530

    v

---
+++ untitled1/src/vignesh.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class vignesh {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        String output="";
+        int i=0;
+        for(i=0;i<input.length();i++){
+            if(input.charAt(i)>='a' && input.charAt(i)<='z'){
+                output+=Character.toUpperCase(input.charAt(i));
+            }
+            else{
+                output+=Character.toLowerCase(input.charAt(i));
+            }
+        }
+        System.out.println(output);
+
+    }
+}
